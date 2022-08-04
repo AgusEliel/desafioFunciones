f755e68f52985553421a4d7a8d33ce7080d56e89
Agus Eliel <98791637+AgusEliel@users.noreply.github.com>
2022-08-04 10:26:27 -0300

    fix

--- index.js
+++ index.js
@@ -1,81 +1,15 @@
-/* function sumar(num1, num2) {
-    console.log("resultado", num1 + num2)
+let nombreAlumno = prompt("ingrese el nombre del alumno");
+let tri1 = 0;
+let tri2 = 0;
+let tri3 = 0;
+let trimestres = parseFloat(3);
+let promediar = 0;
+
+function promedioTrimestre() {
+    tri1 = parseFloat(prompt("Primer Trimestre"));
+    tri2 = parseFloat(prompt("Segundo Trimestre:"));
+    tri3 = parseFloat(prompt("Tercer Trimestre:"));
+
+    promediar = (tri1 + tri2 + tri2) / 3;
+    console.log("El promedio de", nombreAlumno + promediar);
 }
-
-function multiplicar(num1, num2) {
-    console.log("resultado", num1 * num2)
-}
-
-function restar(num1, num2) {
-    console.log("resultado", num1 - num2)
-}
-
-function dividir(num1, num2) {
-    console.log("resultado", num1 / num2)
-}
-
-
-function calculadora(primerNumero, segundoNumero, operacion) {
-    switch (operacion) {
-        case "+":
-            return primerNumero + segundoNumero;
-            break;
-        case "-":
-            return primerNumero - segundoNumero;
-            break;
-        case "*":
-            return primerNumero * segundoNumero;
-            break;
-        case "/":
-            return primerNumero / segundoNumero;
-            break;
-        default:
-            return 0;
-            break;
-    }
-}
-console.log(calculadora(10, 5, "*")); */
-
-
-
-/* function promedio (primerTrimestre, segundoTrimestre, tercerTrimestre){
-    let notaFinal = primerTrimestre + segundoTrimestre + tercerTrimestre
-} */
-
-/* let nombreAlumno = prompt("Ingrese el nombre del alumno");
-
-let primerTrimestre = parseInt(prompt("Nota del primer trimestre"));
-let segundoTrimestre = parseInt(prompt("Nota del primer trimestre"));
-let tercerTrimestre = parseInt(prompt("Nota del primer trimestre"));
-
-let notaFinal = primerTrimestre + segundoTrimestre + tercerTrimestre
-
-let promedio = notaFinal / 3
-
-if (notaFinal == 21){
-    console.log(nombreAlumno + " esta aprobado con un promedio de " + promedio)
-}
-
-else if (notaFinal > 21){
-    console.log(nombreAlumno + " esta aprobado con un promedio de " + promedio)
-}
-
-else if (notaFinal < 21){
-    console.log(nombreAlumno + " no pudo aprobar porque su promedio fue " + promedio)
-} */
-
-
-function CalcularPromedio(){
-    let v1 = document.getElementById("n1").value;
-    let v2 = document.getElementById("n2").value;
-    let v3 = document.getElementById("n3").value;
-    let pro = (parseFloat(v1)+parseFloat(v2)+parseFloat(v3))/3;
-    document.getElementById("promedio").innerHTML=pro;
-}
-
-
-
-
-
-
-
